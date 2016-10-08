5acb70ba4b6975289213434a461a35630f9950ef
Edward Webb <spider.pole@live.com>
2016-10-09 10:22:17 +1100

    Set up basic layout

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, QPushButton, QApplication, QVBoxLayout, QHBoxLayout)
+from PyQt5.QtWidgets import (QWidget, QListWidget, QLabel, QLineEdit, QPushButton, QApplication, QGridLayout)
 from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtGui import QIcon, QWindow
 from PyQt5.QtCore import Qt, QEvent
 import sys, subprocess, socket, time, os
 
 class MainClass(QWidget):
-    def __init__(self):
-        super().__init__()
+	def __init__(self):
+		super().__init__()
 
-        self.mainUI()
-    def mainUI(self):
-    	self.mainLayout = QVBoxLayout()
-    	self.setGeometry(300, 300, 300, 300)
-    	self.setWindowTitle("Quick Access")
-    	self.show()
+		self.mainUI()
+	def mainUI(self):
+		self.listItem = QListWidget(self)
+		self.lineBox = QLineEdit()
+		self.pathLabel = QLabel("Path: ")
+		self.addButton = QPushButton("Add")
+		self.mainLayout = QGridLayout()
+		# self.mainLayout.setSpacing()
+		self.mainLayout.addWidget(self.pathLabel, 2, 0)
+		self.mainLayout.addWidget(self.listItem, 1, 0, 1, 3)
+		self.mainLayout.addWidget(self.addButton, 2, 2)
+		self.mainLayout.addWidget(self.lineBox, 2, 1)
+		self.setLayout(self.mainLayout)
+		self.setGeometry(300, 300, 300, 300)
+		self.setWindowTitle("Quick Access")
+		self.show()
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
