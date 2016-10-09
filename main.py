aa245d76a05ccfa96f7bf534bff30554813d1716
Edward Webb <spider.pole@live.com>
2016-10-09 11:43:15 +1100

    Added file dialog functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import (QWidget, QListWidget, QLabel, QLineEdit, QPushButton, QApplication, QGridLayout)
+from PyQt5.QtWidgets import (QWidget, QListWidget, QListWidgetItem, QLabel, QLineEdit, QPushButton, QApplication, QGridLayout, QFileDialog)
 from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtGui import QIcon, QWindow
 from PyQt5.QtCore import Qt, QEvent
@@ -9,17 +9,25 @@ class MainClass(QWidget):
 		super().__init__()
 
 		self.mainUI()
+	def browse(self):
+		fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
+		self.lineBox.setText(fname[0])
+	def addToList(self):
+		print("%s" % self.lineBox.text)
 	def mainUI(self):
-		self.listItem = QListWidget(self)
+		self.listWidget = QListWidget(self)
 		self.lineBox = QLineEdit()
+		self.lineBox.setReadOnly(True)
 		self.pathLabel = QLabel("Path: ")
 		self.addButton = QPushButton("Add")
+		self.addButton.clicked.connect(self.addToList)
 		self.removeButton = QPushButton("Remove")
 		self.browseButton = QPushButton("Browse")
+		self.browseButton.clicked.connect(self.browse)
 		self.mainLayout = QGridLayout()
 		# self.mainLayout.setSpacing()
 		self.mainLayout.addWidget(self.pathLabel, 2, 0)
-		self.mainLayout.addWidget(self.listItem, 1, 0, 1, 5)
+		self.mainLayout.addWidget(self.listWidget, 1, 0, 1, 5)
 		self.mainLayout.addWidget(self.addButton, 2, 4)
 		self.mainLayout.addWidget(self.removeButton, 2, 3)
 		self.mainLayout.addWidget(self.browseButton, 2, 2)
